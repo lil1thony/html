a1d0245ea8a012d19f7a0e515995107ea63963f6
lil1thony <thony200805@gmail.com>
2024-04-05 10:59:36 -0600

    array

---
+++ src/array.js
@@ -0,0 +1,60 @@
+document.querySelector('#header').innerHTML = "estoy leyendo el documento array" 
+
+var cesta = ["fresas", "pera", "cereza"]
+
+console.log(cesta)
+
+cesta[0] = "uva" // para modificar el valor de una posicion 
+console.log(cesta)
+
+console.log(cesta.length) //ver el tamaño del array 
+
+cesta.push("naranja") // agrega un elemento al array
+console.log(cesta)
+
+cesta.pop() // elimina el ultimo elemento del array 
+console.log(cesta)
+
+cesta.push("naranja")
+cesta.push("piña")
+console.log(cesta)
+
+/*
+el segundo paràrametro (1) indica cuantos elementos eliminar  
+desde el indice prporcionado (2)
+*/
+cesta.splice(2,2)
+
+
+console.log(cesta[2])
+
+
+var cestaVacia = [] // crear un array vacio
+
+
+
+
+
+var opcion = prompt("1. ver elmentos de la cesta\n2. Agregar una fruta a la cesta\n3. eliminar la fruta de la cesta")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
