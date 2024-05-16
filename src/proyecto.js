35a74042a6f67525d89ead47533d4fd3dd5fdb03
lil1thony <thony200805@gmail.com>
2024-05-16 15:57:22 -0600

    proyecto2

--- src/proyecto.js
+++ src/proyecto.js
@@ -1,28 +1,54 @@
+// Definimos el arreglo vacio
 let arreglo = []
 
-var largo = parseInt(prompt("Digite la cantidad de elementos delo arreglo"))
+var largo = parseInt(prompt("Digite la cantidad de elementos del arreglo"))
 
-function construir(largo) {
+function construirarreglo(largo) {
     arreglo.length = largo
 }
 //llamar funcion costruir 
-construir(largo)
+construirarreglo(largo);
 
 function llenararreglo() {
-    for (let i = 0; i < largo; i++) {
+    for (let i = 0; i < arreglo.length; i++) {
         let numeroAleatorio = Math.floor(Math.random() * 20) + 1
         arreglo[i] = numeroAleatorio
     }
+    console.log(arreglo)
+}
+
+function promedioarreglo() {
+   let sumaTotal = 0;
+   for(let i = 0; i < arreglo.length; i++){
+      sumaTotal = sumaTotal + arreglo [i];
+   }
+   return sumaTotal / arreglo.length 
+}
+
+function mayoresPromedios(){
+  let promedio = promedioarreglo();
+  let cantidaDeElementos = 0;
+  for(let i = 0; i < arreglo.length; i++){
+    if (arreglo[i] > promedio){
+        cantidaDeElementos++;
+    }
+  }
+  return cantidaDeElementos
+}
+
+function sumaValor(aumentarArreglo){
+    arreglo.length = arreglo.length; +aumentarArreglo
 }
 
 do {
-    var opcion = parseInt(prompt("1. llenar arrelgo"))
+  var opcion = parseInt(prompt("1. llenar arrelgo\n.2 sacar el promedio del arreglo\n 3. sacar el total de mayores al promedio\n 4.aumentar arreglo\n 5. sumar impares\n 6. nostrar el nùmero mayor del arreglo\n 7. Mostrar el Nùmero mayor diferencia en valor absoluto entre dos celdas consecutivas del arreglo\n 8. Buscar nùmero en el arreglo\n 9. Buscarposiciòn de un arreglo\n 10. inverrtir arreglo\n 11. sustituir el valor de una posicion specifica\n 12. mostrar hilera del arreglo\n 13. finalizar aplicacion"))
 
+   
     switch (opcion) {
         case 1:
             llenararreglo()
-        break
+            break
     }
 
 
-} while (true)
+} while (opcion != 0)
